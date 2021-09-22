53ddaeba8643d7f256fc262a42e05a85d13ec6fc
Your Name <dmqthai15@gmail.com>
2021-09-22 22:19:33 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import WordCard from './WordCard';
+
+
+  function App(){
+    return(
+      <WordCard value="PUMPONG" />
+ );
+}
+export default App;
